a749079dd01ae8389244175d5437ad1ba97adf62
brayooh <brianndichu.bn@gmail.com>
2019-09-05 15:13:09 +0300

    deleted html files

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-//BUSINESS LOGIC
-function Doctor (firstName,lastName,age,gender,hospital,id) {
-    this.firstName=firstName;
-    this.lastName=lastName;
-    this.age=age;
-    this.gender=gender;
-    this.hospital=hospital;
-    this.id=id;
-}
